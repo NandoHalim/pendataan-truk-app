ffc4b14c5781f95271346747d197ba0753fa028d
Nando Halim <nando@example.com>
2025-10-11 09:55:27 +0700

    fix(entry): point main.jsx to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './views/DashboardView.jsx'
+import App from './App.jsx'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import theme from './theme'
 
